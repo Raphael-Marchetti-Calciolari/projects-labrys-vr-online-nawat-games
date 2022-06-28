ef31b4c9465d9732c9dd2dcc2e6d78f135fec028
Raphael Marchetti Calciolari <79259612+raphazik4@users.noreply.github.com>
2022-06-28 15:14:22 -0300

    Fixed compilation errors

--- LabrysVROnline-UnityProject/Assets/Scripts/Sword/Sword.cs
+++ LabrysVROnline-UnityProject/Assets/Scripts/Sword/Sword.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 public class Sword : MonoBehaviour
 {
     public static string swordClassTag = "Sword";
+    public static event Action<float, GameObject> NotifySwordCollision;
+    [SerializeField] private float damage;
 
     private void Start()
     {
         if (!gameObject.tag.Equals(swordClassTag)) Console.WriteLine($"Sword instance ${gameObject} has no tag attributed!");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerStatus>())
+        {
+            NotifySwordCollision?.Invoke(damage, collision.gameObject);
+        }
+        else if (collision.gameObject.tag.Equals(Sword.swordClassTag));
+            //Sabotage();
     }
 
 }
